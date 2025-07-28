95e4517510d2a8c1d211ddf6fe51c81e02d5b30a
shinyboy <146899115+Joker-Pro-Max@users.noreply.github.com>
2025-07-28 15:20:51 +0800

    完成注册

--- basalt_oa/account/application/use_cases.py
+++ basalt_oa/account/application/use_cases.py
@@ -1,1 +1,18 @@
-# application/use_cases.py
+from account.domain.services import UserDomainService
+from account.infrastructure.repositories import DjangoUserRepository
+
+class RegisterUserUseCase:
+    def __init__(self):
+        self.user_repo = DjangoUserRepository()
+
+    def execute(self, username, email, phone, password, system_code):
+        return UserDomainService(self.user_repo).register_user(username, email, phone, password, system_code)
+
+
+class LoginUserUseCase:
+    def __init__(self):
+        self.user_repo = DjangoUserRepository()
+
+    def execute(self, account, password, system_code):
+        service = UserDomainService(self.user_repo)
+        return service.authenticate_user(account, password, system_code)
